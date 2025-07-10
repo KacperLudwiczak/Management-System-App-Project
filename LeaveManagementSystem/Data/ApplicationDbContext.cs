2a1c53659bc489d5138132401eb98f86088fc00a
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-07-10 14:13:53 +0200

    Add LeaveType Table

--- LeaveManagementSystem/Data/ApplicationDbContext.cs
+++ LeaveManagementSystem/Data/ApplicationDbContext.cs
@@ -7,6 +7,7 @@ public class ApplicationDbContext : IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
-
     }
+
+    public DbSet<LeaveType> LeaveTypes { get; set; }
 }
---
+++ LeaveManagementSystem/Data/LeaveType.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagementSystem.Data;
+
+public class LeaveType
+{
+    public int Id { get; set; }
+    [MaxLength(150)]
+    public required string Name { get; set; }
+    public int NumberOfDays { get; set; }
+}
