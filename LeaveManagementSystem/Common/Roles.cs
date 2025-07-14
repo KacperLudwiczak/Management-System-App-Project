049bf4f73296e9fc1be9dfaab94118dfbc1b0144
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-07-14 16:17:39 +0200

    Add Roles

---
+++ LeaveManagementSystem/Common/Roles.cs
@@ -0,0 +1,6 @@
+public static class Roles
+{
+    public const string Administrator = "Administrator";
+    public const string Supervisor = "Supervisor";
+    public const string Employee = "Employee";
+}
