32716e759a116b555e71aa9ab2be9c5e1490f416
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-07-15 15:57:06 +0200

    Create LeaveAllocationsService

---
+++ LeaveManagementSystem/Services/LeaveAllocations/ILeaveAllocationsService.cs
@@ -0,0 +1,12 @@
+using LeaveManagementSystem.Models.LeaveAllocations;
+
+namespace LeaveManagementSystem.Services.LeaveAllocations;
+
+public interface ILeaveAllocationsService
+{
+    Task AllocateLeave(string employeeId);
+    Task<EmployeeAllocationVM> GetEmployeeAllocations(string? userId);
+    Task<LeaveAllocationEditVM> GetEmployeeAllocation(int allocationId);
+    Task<List<EmployeeListVM>> GetEmployees();
+    Task EditAllocation(LeaveAllocationEditVM allocationEditVm);
+}
