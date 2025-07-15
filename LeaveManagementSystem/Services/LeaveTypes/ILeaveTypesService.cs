90c31c391afe54ec963176f0fb6e44ad830dd9a6
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-07-15 16:00:39 +0200

    Change LeaveTypesService

--- LeaveManagementSystem/Services/LeaveTypes/ILeaveTypesService.cs
+++ LeaveManagementSystem/Services/LeaveTypes/ILeaveTypesService.cs
@@ -1,12 +1,13 @@
 using LeaveManagementSystem.Models.LeaveTypes;
 
-namespace LeaveManagementSystem.Services;
+namespace LeaveManagementSystem.Services.LeaveTypes;
 
 public interface ILeaveTypesService
 {
     Task<bool> CheckIfLeaveTypeNameExists(string name);
     Task<bool> CheckIfLeaveTypeNameExistsForEdit(LeaveTypeEditVM leaveTypeEdit);
     Task Create(LeaveTypeCreateVM model);
+    Task<bool> DaysExceedMaximum(int leaveTypeId, int days);
     Task Edit(LeaveTypeEditVM model);
     Task<T?> Get<T>(int id) where T : class;
     Task<List<LeaveTypeReadOnlyVM>> GetAll();
