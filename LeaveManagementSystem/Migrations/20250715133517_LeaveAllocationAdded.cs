1af6856b50be5a7a6b7fd481bdade26917e8df9e
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-07-15 15:37:33 +0200

    Add Migrations

---
+++ LeaveManagementSystem/Migrations/20250715133517_LeaveAllocationAdded.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeaveManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class LeaveAllocationAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Periods",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StartDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    EndDate = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Periods", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LeaveAllocations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LeaveTypeId = table.Column<int>(type: "int", nullable: false),
+                    EmployeeId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    PeriodId = table.Column<int>(type: "int", nullable: false),
+                    Days = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LeaveAllocations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LeaveAllocations_AspNetUsers_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_LeaveAllocations_LeaveTypes_LeaveTypeId",
+                        column: x => x.LeaveTypeId,
+                        principalTable: "LeaveTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LeaveAllocations_Periods_PeriodId",
+                        column: x => x.PeriodId,
+                        principalTable: "Periods",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "admin-user-id",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "935bd57d-ea10-4a01-853c-77cd46c8a40e", "AQAAAAIAAYagAAAAEKrwZApRELB+TJUus6NAPlIy5MbWaGiFBUuYoAdXAejBdKy2quDaTnbP/OUibQpm3Q==", "8c383ddb-fa2b-4357-9657-927164d0d4dd" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveAllocations_EmployeeId",
+                table: "LeaveAllocations",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveAllocations_LeaveTypeId",
+                table: "LeaveAllocations",
+                column: "LeaveTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveAllocations_PeriodId",
+                table: "LeaveAllocations",
+                column: "PeriodId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LeaveAllocations");
+
+            migrationBuilder.DropTable(
+                name: "Periods");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "admin-user-id",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "99fce01a-23f9-4868-8242-80455a915554", "AQAAAAIAAYagAAAAEHb5zeFIGrzUA+kWe894UHxYu2Y6BtorwYNctBllHf2C05aLwKVO245aeULPN6jYaw==", "240f1c6e-691f-4f49-ac8f-b490ed067dbb" });
+        }
+    }
+}
